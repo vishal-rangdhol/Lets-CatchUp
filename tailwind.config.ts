9a2d57ac62849bf5c3274c4740b90ed1786cf2e4
VISHAL RANGDHOL <vishalRangdhol@gmail.com>
2026-03-12 04:34:42 +0000

    apply poppins for all headings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
-        headline: ['Roboto', 'sans-serif'],
+        headline: ['Poppins', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
